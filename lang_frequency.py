bafc56d6c207ea7f52f996abbad56ed46ac61820
evro23x <evro23x@mail.ru>
2016-12-31 10:54:51 +0300

    remane variables

--- lang_frequency.py
+++ lang_frequency.py
@@ -8,19 +8,19 @@ def load_data(filepath):
         return shops.read()
 
 
-def get_most_frequent_words(text):
-    requent_words = {}
-    for word in text:
+def get_most_frequent_words(data):
+    dict_words = {}
+    for word in data:
         word = word.lower()
-        if word in requent_words.keys():
-            requent_words[word] += 1
+        if word in dict_words.keys():
+            dict_words[word] += 1
         else:
-            requent_words[word] = 1
-    return sorted(requent_words.items(), key=operator.itemgetter(1), reverse=True)
+            dict_words[word] = 1
+    return sorted(dict_words.items(), key=operator.itemgetter(1), reverse=True)
 
 
 if __name__ == '__main__':
-    data = load_data(sys.argv[1]).split()
-    pairs = get_most_frequent_words(data)[0:9]
-    for pair in pairs:
+    data_from_file = load_data(sys.argv[1]).split()
+    frequent_words = get_most_frequent_words(data_from_file)[0:9]
+    for pair in frequent_words:
         print("Слово '%s' встречалось в тексте %s раз(а)" % (pair[0], pair[1]))
