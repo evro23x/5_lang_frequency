46584fb7de2cb1135c9739b10e930e1d3c1070e1
Evgeny Gurov <evro23x@mail.ru>
2017-01-06 14:47:25 +0300

    all text to lowercase & add const & refactoring with collections.Counter

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,26 +1,26 @@
+import os
 import sys
-import operator
 import codecs
+from collections import Counter
+
+MAX_COUNT = 10
 
 
 def load_data(filepath):
+    if not os.path.exists(filepath):
+        return None
     with codecs.open(filepath, encoding='UTF-8') as shops:
-        return shops.read()
+        return shops.read().lower()
 
 
 def get_most_frequent_words(data):
-    dict_words = {}
-    for word in data:
-        word = word.lower()
-        if word in dict_words.keys():
-            dict_words[word] += 1
-        else:
-            dict_words[word] = 1
-    return sorted(dict_words.items(), key=operator.itemgetter(1), reverse=True)
+    return Counter(data).most_common(MAX_COUNT)
 
 
 if __name__ == '__main__':
     data_from_file = load_data(sys.argv[1]).split()
-    frequent_words = get_most_frequent_words(data_from_file)[0:9]
+    frequent_words = get_most_frequent_words(data_from_file)
     for pair in frequent_words:
         print("Слово '%s' встречалось в тексте %s раз(а)" % (pair[0], pair[1]))
+
+
